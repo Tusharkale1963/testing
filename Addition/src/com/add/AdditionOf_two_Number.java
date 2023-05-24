9300e9ef4f48ac4243c769a732e5d9d4eebd92be
tusharkale1963 <tusharkale1963@gmail.com>
2023-05-24 13:00:47 +0530

    first commit

---
+++ Addition/src/com/add/AdditionOf_two_Number.java
@@ -0,0 +1,15 @@
+package com.add;
+
+public class AdditionOf_two_Number {
+ static void m1() {
+	 int a=10;
+	 int b=20;
+	int  sum =a+b;
+	System.out.println(sum);
+ }
+	public static void main(String[] args) {
+		m1();
+
+	}
+
+}
